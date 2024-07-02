3b9a73c64003883c0dc9f30d707e19f32c1b0b21
hardikpant03 <hardikpant8@gmail.com>
2024-07-02 14:35:10 +0530

    Update the file and add the path

---
+++ waybuddy/src/components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar__logo">
+            </div>
+            <div className="navbar-container">
+                <div className="navbar-brand">
+                    <img src="https://i.imgur.com/Xv0aX0Q.png" alt="Logo" />
+                    <span>ACME</span>
+                </div>
+                <ul className="navbar-menu">
+                <li className="navbar-item">
+                        <Link className="bar" to="/">Home</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link className="bar" to="/about">About</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link className="bar" to="/navlink">nav link</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link className="bar" to="/contribute">Contribute</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link className="bar" to="/contact">Contact</Link>
+                    </li>
+                </ul>
+                {/* <div className="navbar-cart"> */}
+                    {/* <a href="#"><i className="fas fa-shopping-cart"></i><span>3</span></a> */}
+                {/* </div> */}
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ waybuddy/src/pages/Contact/contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './contact.css'
+import Header from "../../components/Header/Header";
+const contact = ()=>{
+    return(
+        <>
+        <Header/>
+        <div className="first">
+            <h1>Contact Us, I am Hardik Pant</h1>
+        </div>
+        </>
+    );
+};
+
+export default contact;
