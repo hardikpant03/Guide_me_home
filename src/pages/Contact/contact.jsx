1565088d64e03d56f0fddb11c13418f88020591a
hardikpant03 <hardikpant8@gmail.com>
2024-07-06 00:43:33 +0530

    Work in Font

--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import './contact.css'
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/footer";
-
+import { FaRegCommentAlt} from "react-icons/fa";
+import { BiCommentError } from "react-icons/bi";
 const contact = () => {
     return(
         <>
         <Header/>
         <div className="container1">
       <div className="contact-section">
-        <div className="heading"><h1>Contact WayBuddy for Support</h1></div>
+          <h1>Contact WayBuddy for Support</h1>
         <div className="contact-options">
           <div className="contact-block">
-            <div className="logo" style={{color:"black"}}>logo attach here</div>
+            <div className="logo" style={{color:"black"}}><FaRegCommentAlt style={{fontSize:"3rem"}} /></div>
             <div className="other">
             <h2>Contact Us</h2>
             <p style={{color:"black"}}>Just want to say hi? We'd love to hear from you. We love our customers and community!</p>
@@ -20,7 +21,7 @@ const contact = () => {
             </div>
           </div>
           <div className="contact-block">
-            <div className="logo" style={{color:"black"}}>logo attach here</div>
+            <div className="logo" style={{color:"black"}}><BiCommentError style={{fontSize:"3rem"}}/></div>
             <div className="other"><h2>Get Support</h2>
             <p style={{color:"black"}}>Have an issue with an order or with a product you purchased from us? Fill out our support form.</p>
             <button className="contact-button">Contact Support</button></div>
@@ -35,8 +36,10 @@ const contact = () => {
         <p>Acme Outdoors</p>
         <p>123 Rainy Street</p>
         <p>Oklahoma City, OK 73129</p>
-        <p><strong>General Inquiries:</strong> (405) 555-5555</p>
-        <p><strong>Customer Support:</strong> (405) 555-5556</p>
+        <div className="ppp">
+        <p className='enq'><strong>General Inquiries:</strong> (405) 555-5555</p>
+        <p className='enq'><strong>Customer Support:</strong> (405) 555-5556</p>
+        </div>
       </div>
       <div className="contact-form">
         <h2>Contact Form</h2>
--- src/pages/Contribute/contribute.jsx
+++ src/pages/Contribute/contribute.jsx
@@ -3,16 +3,48 @@ import './contribute.css'
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/footer";
 
+const System = () => {
+    return (
+      <div className="topperbahi">
+        <div className="overlay">
+          <h1>Here at Acme Outdoors</h1>
+          <h2>every dollar counts</h2>
+          <p>
+            Acme Outdoors is more than just a company, we're a community of people who care for one another and for our city.
+            During this time, due to shelter in place orders, only a select few of our staff are able to work. Any donations
+            you make to Acme will help make sure our employees are cared for and can stay safe in these uncertain times.
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
+  const DonationButton = ({ amount }) => {
+    return (
+      <button className="donationButton" onClick={()=>{ alert("Add this feature after word ")}}>Donate {amount} Rupay</button>
+    );
+  };
+  
+  const DonationSection = () => {
+    const donationAmounts = [10000, 5000, 2500, 1500, 500];
+  
+    return (
+      <section className="donationSection">
+        {donationAmounts.map(amount => (
+          <DonationButton key={amount} amount={amount} />
+        ))}
+      </section>
+    );
+  };
+
 const contribute = ()=>{
     return(
-        <>
+        <div className="Con">
         <Header />
-        <div className="first">
-            <h1>About Contribute</h1>
-            <p className="about">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique eaque placeat ex quod earum ipsam soluta? Minima maiores omnis ipsam itaque similique eos iusto deleniti. Velit aspernatur, et beatae cumque dolores impedit architecto. Explicabo ipsam, fugiat quaerat laboriosam rerum cumque?</p>
-        </div>
+        <System />
+        <DonationSection />
         <Footer />
-        </>
+        </div>
     );
 };
 
